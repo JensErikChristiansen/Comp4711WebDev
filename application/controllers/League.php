50318be80ba8dee9bedb43afc817c1551f424529
Jens Christiansen <jens@jenschristiansen.com>
2015-10-03 14:27:49 -0700

    -added Teams model -set up league view with table displaying Teams model data

--- application/controllers/League.php
+++ application/controllers/League.php
@@ -14,9 +14,25 @@
 class League extends Application {
     public function index()
     {
-
-            //$this->load->view('welcome');
+    	$this->load->model('teams');
+    	$this->data['teams'] = $this->teams->all();
+        //$this->load->view('welcome');
         $this->data['pagebody'] = 'league';
+
+        $parms = array(
+        	'table_open' => '<table class="league">',
+        	'cell_alt_start' => '<td class="oneteam">',
+        	'cell_start' => '<td>',
+        	'cell_end' => '</td>',
+        	'row_start' => '<tr>',
+        	'row_end' => '</tr>',
+
+        	);
+
+        $this->table->set_template($parms);
+        //$this->data['thetable'] = $this->table;
+
+
         $this->render();
     }
 }
