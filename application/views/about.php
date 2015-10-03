a064fdcaef974ae329608e28dd94334d08026094
rwubs <rosannawubs@gmail.com>
2015-10-03 12:02:18 -0700

    added the views for the appropriate pages. Pair programming with Nadia Dobrianskaia.

---
+++ application/views/about.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <p>Project Steelers is a BCIT CST COMP4711 course assignment.</p>
+        <p>Any use of names, and / or media falls under the Fair Used Guidelines.</p>
+    </body>
+</html>
