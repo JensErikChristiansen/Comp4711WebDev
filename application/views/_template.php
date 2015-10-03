11bffffbb2e6ee2760918cda9d74a5bccf3a7181
Jens Christiansen <jens@jenschristiansen.com>
2015-10-03 15:30:40 -0700

    incorporated bootstrap

--- application/views/_template.php
+++ application/views/_template.php
@@ -10,21 +10,30 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <title>{pagetitle}</title>
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
     </head>
     <body>
         <div id="header">
-            <span class="mynav">
-                <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/league">League</a></li>
-                    <li><a href="/roster">Roster</a></li>
-                    <li><a href="/about">About</a></li>
-                </ul>
-            </span>
+            <nav class="navbar navbar-default">
+                <div class="container-fluid">
+                    <ul class="nav navbar-nav">
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/league">League</a></li>
+                        <li><a href="/roster">Roster</a></li>
+                        <li><a href="/about">About</a></li>
+                    </ul>
+                </div>
+            </nav>
        </div>
        <div id="content">
-           
+            <div class="container">
              {content}
+            </div>
         </div>
     </body>
 </html>
--- application/views/league.php
+++ application/views/league.php
@@ -5,7 +5,7 @@ To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
 <h1>The League</h1>
-<table>
+<table class="table">
 	<tr>
 		<td>Logo</td>
 		<td>ID</td>
