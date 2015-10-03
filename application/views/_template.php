26de960e1df751857542fe6d340964893618306f
Nadia Dobrianskaia <nadezhdadobrianskaia@gmail.com>
2015-10-03 11:27:02 -0700

    Steelers homepage added pair programming Rosanna and Nadia

---
+++ application/views/_template.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<!--
+
+Authors:  Pair programming Rosanna and Nadia
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>{pagetitle}</title>
+    </head>
+    <body>
+        <div id="header">
+                <span class="myhead">Simple Image Gallery</span>
+                <span class="mynav">
+                    <ul>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/league">League</a></li>
+                        <li><a href="/roster">Roster</a></li>
+                        <li><a href="/about">About</a></li>
+                    </ul>
+                </span>
+            </div>
+       <div id="content">
+           
+             {content}
+        </div>
+    </body>
+</html>
