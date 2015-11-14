57a25e7826276ffc819fc2dd6c8ac41228b493c7
Nadia Dobrianskaia <nadezhdadobrianskaia@gmail.com>
2015-11-14 15:44:34 -0800

    addign player view

--- application/views/_gallery.php
+++ application/views/_gallery.php
@@ -2,7 +2,7 @@
 ?>
 <div class="row">
 	<div class="col-lg-3 col-md-4 col-xs-6 thumb">
-		<a class="thumbnail" href="player/{ID}">
+		<a class="thumbnail" href="/player/display_player/{ID}">
 			<img class="img-responsive" src="/assets/data/img/{Photo}">
 			<label>{Name}</label>
 		</a>
---
+++ application/views/player.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+
+<div class="row">
+    <div class="col-xs-6">
+        <img src="/assets/data/img/{Photo}">
+    </div>
+    <div class="col-xs-6">
+        <ul>
+            <li>{Name}</li>
+            <li>{PlayerNo}</li>
+            <li>{Pos}</li>
+            <li>{Status}</li>
+            <li>{Height}</li>
+            <li>{Weight}</li>
+            <li>{Birthdate}</li>
+            <li>{Experience}</li>
+            <li>{College}</li>
+            <li>{Code}</li>
+        </ul>
+        
+    </div>
+</div>
+        
+
