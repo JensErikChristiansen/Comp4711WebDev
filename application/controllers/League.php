ee8424f195f427248188795efdcbd597bce0e7b2
rosannawubs <rosannawubs@gmail.com>
2015-11-14 19:50:57 -0800

    fixed logos on the league page. Added an orderby feature

--- application/controllers/League.php
+++ application/controllers/League.php
@@ -15,10 +15,19 @@ class League extends Application {
     public function index()
     {
     	//$this->load->model('teams');
-    	$this->data['teams'] = $this->teams->all();
+    	//$this->data['teams'] = $this->teams->all_ordered_by('W');
         //$this->load->view('welcome');
         $this->data['pagebody'] = 'league';
 
+        //first see if the session was set if not set display table layout               
+        if(isset($_SESSION['orderby'])){
+            $orderby = $this->session->userdata('orderby');
+            $this->data['teams'] = $this->teams->all_ordered_by($orderby);
+        }
+        else
+            $this->data['teams'] = $this->teams->all_ordered_by('Code');
+        
+        
         $parms = array(
         	'table_open' => '<table class="league">',
         	'cell_alt_start' => '<td class="oneteam">',
@@ -34,5 +43,20 @@ class League extends Application {
 
 
         $this->render();
+    }
+    
+    function orderby($orderby){
+        if($orderby == "name"){
+             $this->session->set_userdata('orderby', 'TeamName');
+        }
+        else if($orderby == "wins") {
+            $this->session->set_userdata('orderby', 'W');
+        }
+        else if($orderby == "losses") {
+            $this->session->set_userdata('orderby', 'L');
+        }
+        else {
+            $this->session->set_userdata('orderby', 'Code');
+        }
     }
 }
